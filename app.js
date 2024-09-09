1149a6a08eccdeaa239a419f6bee299f3830d726
Birajit Debbarma <birajitdebbarma@gmail.com>
2024-09-09 21:32:35 +0530

    as

---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const hostingController = require('./controller/hostingController');
+
+const app = express();
+
+// Set up view engine (EJS for simple templating)
+app.set('view engine', 'ejs');
+
+// Set up static files (CSS, JS)
+app.use(express.static('public'));
+
+// Parse form data
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Home Route
+app.get('/', hostingController.showDashboard);
+app.get('/dashboard', hostingController.hostingDashboard);
+app.get('/setting', hostingController.settingPage);
+app.get('/billing', hostingController.billingPage);
+app.get('/pay', hostingController.payPage);
+app.get('/support', hostingController.supportPage);
+
+// Add domain route
+app.post('/add-domain', hostingController.addDomain);
+
+// Start server
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}`);
+});
---
+++ controller/hostingController.js
@@ -0,0 +1,35 @@
+const domains = []; // Simple in-memory storage for domains
+
+// Show the main dashboard
+exports.showDashboard = (req, res) => {
+  res.render('index', { domains });
+};
+
+// Add a domain
+exports.addDomain = (req, res) => {
+  const domainName = req.body.domain;
+  if (domainName) {
+    domains.push(domainName);
+  }
+  res.redirect('/');
+};
+
+exports.hostingDashboard = (req, res) => {
+    res.render('dashboard');
+}
+
+exports.settingPage = (req, res) => {
+    res.render('setting'); // This renders the 'about.ejs' view
+  };
+
+exports.billingPage = (req, res) => {
+    res.render('billing'); // This renders the 'about.ejs' view
+  };
+
+exports.payPage = (req, res) => {
+    res.render('pay'); // This renders the 'about.ejs' view
+  };
+
+exports.supportPage = (req, res) => {
+    res.render('support'); // This renders the 'about.ejs' view
+  };
